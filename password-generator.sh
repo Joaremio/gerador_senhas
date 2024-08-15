a0c25d5748b3365eb2fbd9de7a84638e0f042504
Bernardo <josepontobernardo@gmail.com>
2024-08-14 21:34:07 -0300

    Mudando a função show_help() para a nova versão

--- password-generator.sh
+++ password-generator.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # Função para exibir a ajuda
 show_help(){
-  echo "Bem vindo ao password-generator! Versão 1.0, (c) 2024, Fulano de Tal, DIMAp, UFRN"
-  echo "Uso: ./password-generator.sh [OPÇÕES]"
+  echo "Bem vindo ao password generator! Versão 2.0, (c) 2024, Cicrano, DIMAp, UFRN"
+  echo "Uso: ./password-generator.sh [OPTIONS]"
   echo "Opções: "
-  echo "  -l [COMPRIMENTO] : comprimento da senha"
-  echo "  -u               : incluir letras maiúsculas"
-  echo "  -d               : incluir números"
-  echo "  -s               : incluir símbolos"
-  echo "  -h               : exibir essa mensagem de ajuda"
-
+  echo "  -l LENGTH  Especifica o tamanho da senha (padrão: 8)"
+  echo "  -u         Inclui letras maiúsculas na senha"
+  echo "  -d         Inclui dígitos na senha"
+  echo "  -s         Inclui símbolos na senha"
+  echo "  -o         Salva a senha gerada em um arquivo"
+  echo "  -n NAME    Adiciona um nome à senha gerada"
+  echo "  -p         Exibe senhas geradas"
+  echo "  -h         Exibe essa mensagem de ajuda"
+  echo ""
   echo "O comportamento padrão do script é gerar uma senha de 8 caracteres minúsculos."
 }
 
